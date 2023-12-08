19e916ae2264503aebc8b7bb99d837a854def626
Fire Ramollo <ramollofire@yahoo.com>
2023-12-08 15:33:44 +0200

    Latest updatesUpdated today

--- BulkieWeb/Program.cs
+++ BulkieWeb/Program.cs
@@ -1,7 +1,17 @@
+using BulkieWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+/* What is Dependency Injection? 
+   -- it is a design pattern in which a class or object has its dependent classes injected rather than directly creating them. 
+      This is done so that we do not have to create, manage or dispose the object and it improves the loose coupling between classes.
+*/
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
